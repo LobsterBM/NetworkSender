b8e32964e39ab917f0c4ff357f7422fa29e8a3b8
Lobster <omar.kraim@student.uclouvain.be>
2019-10-14 16:00:18 +0200

    I want to die

--- Sender/main.c
+++ Sender/main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
-int main() {
-    printf("Hello, World!\n");
+int main(int argc, char *argv[] ) {
+
+    for(int i  = 0 ; i < argc ; i++){
+        printf(argv[i]);
+        printf("\n");
+    }
+
     return 0;
 }
