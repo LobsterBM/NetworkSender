696b65ffb7a63cb7e1a5e31ebaa46d866330323c
Lobster <omar.kraim@student.uclouvain.be>
2019-10-14 13:53:21 +0200

    Clion project creation

---
+++ Sender/cmake-build-debug/CMakeFiles/Sender.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Sender.dir/main.c.o"
+  "Sender.pdb"
+  "Sender"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Sender.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
