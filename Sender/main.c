9213e647325d9b5066b3f081c9f7db02ec8f4534
Lobster <omar.kraim@student.uclouvain.be>
2019-10-16 11:33:21 +0200

    azerty

--- Sender/main.c
+++ Sender/main.c
@@ -1,8 +1,32 @@
 #include <stdio.h>
+#include <string.h>
+
+char * filename;
+char * address;
+int port ;
+
+int ArgReader(int argc, char *argv[]){
+    //TODO argument arror check
+    for(int i = 1 ; i < argc ; i++){
+        if(strstr(argv[i],"-f" ) != NULL){
+            i++;
+            filename = argv[i];
+            //TODO extract filename from position i+1
+        }
+        else{
+            address = argv[i];
+            i++; //port numer
+            port = argv[i];
+        }
+    }
+
+}
+
+
 
 int main(int argc, char *argv[] ) {
 
-    for(int i  = 0 ; i < argc ; i++){
+    for(int i  = 1 ; i < argc ; i++){
         printf(argv[i]);
         printf("\n");
     }
